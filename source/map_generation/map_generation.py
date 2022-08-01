9d85c1b58988b2bfe24d2876a18cf8b0be70d676
Kerstin Hofer <khofer.mmt-m2020@fh-salzburg.ac.at>
2022-08-02 00:40:57 +0200

    Add base structure for CGAN for mapgeneration

---
+++ source/map_generation/map_generation.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+# generator
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+# discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
