e991431d759227772a6ce47fa7dbfc86fa244c1b
Kerstin Hofer <khofer.mmt-m2020@fh-salzburg.ac.at>
2022-09-17 00:42:53 +0200

    Fix renderings and reorganize code.

--- source/mesh_generation/deform_mesh.py
+++ source/mesh_generation/deform_mesh.py
@@ -2,28 +2,46 @@ import drjit as dr
 import mitsuba as mi
 import math
 import numpy as np
+import torch
+import torchvision
 from mitsuba.scalar_rgb import Transform4f as T
+import normal_reparam_integrator
+import depth_reparam_integrator
 
 from torch.utils.tensorboard import SummaryWriter
 
 mi.set_variant('cuda_ad_rgb')
 
-def offsetVerts(params, opt):
+def write_output_renders(render_depth, render_normal, image_name):
+    ref_bpm_normal = mi.util.convert_to_bitmap(render_normal)
+    ref_np_normal = np.transpose(np.array(ref_bpm_normal), (2, 0, 1))
+    ref_bpm_depth = mi.util.convert_to_bitmap(render_depth)
+    ref_np_depth = np.transpose(np.array(ref_bpm_depth), (2, 0, 1))
+    ref_images = np.stack([ref_np_normal, ref_np_depth])
+    writer.add_images(image_name, ref_images)
+
+def offset_verts(params, opt):
     opt['deform_verts'] = dr.clamp(opt['deform_verts'], -0.5, 0.5)
     trafo = mi.Transform4f.translate(opt['deform_verts'].x)
     params['test.vertex_positions'] = dr.ravel(trafo @ initial_vertex_positions)
     params.update()
 
+
+num_epochs = 200
+learning_rate = 0.001
+weight_normal = 0.5
+weight_depth = 0.5
+weight_edge = 0.9
+weight_smooth = 0.01
+
 writer = SummaryWriter("..\\..\\logs")
-aov_integrator = {
-    'type': 'normal_depth',
-    'aovs': "nn:sh_normal,dd.y:depth"
-}
 
-integrator = {
-        'type': 'direct'
+depth_integrator = {
+        'type': 'depth_reparam'
 }
-
+normal_integrator = {
+        'type': 'normal_reparam'
+}
 
 distance = math.tan(math.radians(60))/2
 centroid = np.array([distance, distance, -distance])
@@ -103,46 +121,43 @@ scene = mi.load_dict({
     }
 })
 
+depth_integrator_lodaded=mi.load_dict(depth_integrator)
+normal_integrator_lodaded=mi.load_dict(normal_integrator)
+normal_img_ref = mi.render(ref_scene, seed=0, spp=1024, integrator=normal_integrator_lodaded)
+depth_img_ref = mi.render(ref_scene, seed=0, spp=1024, integrator=depth_integrator_lodaded)
+write_output_renders(depth_img_ref, normal_img_ref, "ref_images")
 
-img_ref = mi.render(ref_scene, seed=0, spp=1024, integrator=mi.load_dict(integrator))
-mi.util.convert_to_bitmap(img_ref)
-#bitmap = mi.Bitmap(img_ref_all, channel_names=['R', 'G', 'B'] + scene.integrator().aov_names())
-#channels = dict(bitmap.split())
-#img_ref = channels['pos']
+normal_img_init = mi.render(scene, seed=0, spp=1024, integrator=normal_integrator_lodaded)
+depth_img_init = mi.render(scene, seed=0, spp=1024, integrator=depth_integrator_lodaded)
+write_output_renders(depth_img_init, normal_img_init, "init_images")
 
 params = mi.traverse(scene)
 print(params)
 initial_vertex_positions = dr.unravel(mi.Point3f, params['test.vertex_positions'])
-initial_vertex_normals = dr.unravel(mi.Point3f, params['test.vertex_normals'])
+#initial_faces = dr.unravel(mi.Point3f, params['test.faces'])
 
-opt = mi.ad.Adam(lr=0.025)
+opt = mi.ad.Adam(lr=learning_rate)
 vertex_count = params['test.vertex_count']
 opt['deform_verts'] = dr.full(mi.Point3f, 0, vertex_count)
 
-img_init = mi.render(scene, seed=0, spp=1024, integrator=mi.load_dict(integrator))
+for epoch in range(num_epochs):
+    offset_verts(params, opt)
 
-ref_bpm = mi.util.convert_to_bitmap(img_ref)
-ref_np = np.transpose(np.array(ref_bpm), (2, 0, 1))
-writer.add_image('ref_images', ref_np)
+    normal_img = mi.render(scene, params, seed=epoch, spp=16, integrator=normal_integrator_lodaded)
+    depth_img = mi.render(scene, params, seed=epoch, spp=16, integrator=depth_integrator_lodaded)
+    if epoch%10 == 0:
+        image_name = "deformed_images" + str(epoch)
+        write_output_renders(depth_img, normal_img, image_name)
 
-for it in range(1000):
-    offsetVerts(params, opt)
-
-    img = mi.render(scene, params, integrator=mi.load_dict(integrator), seed=it, spp=16)
-
-    loss = dr.sum(dr.sqr(img - img_ref)) / len(img)
-
+    depth_loss = dr.sum(dr.sqr(depth_img - depth_img_ref)) / len(depth_img)
+    normal_loss = dr.sum(dr.sqr(normal_img - normal_img_ref)) / len(normal_img)
+    loss = depth_loss * weight_depth + normal_loss * weight_normal
     dr.backward(loss)
 
     opt.step()
 
-    if it%100 == 0:
-        current_bpm = mi.util.convert_to_bitmap(img)
-        current_np = np.transpose(np.array(current_bpm), (2, 0, 1))
-        writer.add_image('current_np' + str(it), current_np)
-
-    writer.add_scalar("loss", loss[0], it)
-    print(f"Iteration {it:02d}: error={loss[0]:6f}", end='\r')
+    writer.add_scalar("loss", loss[0], epoch)
+    print(f"Epochs {epoch:02d}: error={loss[0]:6f} loss_normal={normal_loss[0]:6f} loss_depth={depth_loss[0]:6f}", end='\r')
 
 writer.close()
 mesh = mi.Mesh(
