393e5dc0f6ea5d0e07be87c292e0ab0788b0e974
Kerstin Hofer <khofer.mmt-m2020@fh-salzburg.ac.at>
2022-09-25 23:35:21 +0200

    Adjust Dataset to simulate xyz dataspace for sketch input.

--- source/experiments/test_dataSet.py
+++ source/experiments/test_dataSet.py
@@ -4,15 +4,20 @@ import source.mapgen_dataset.DataSet as DataSet
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision import transforms
+import os
+# Since this is only an experiment failures that may result from this are fine and it is less time consuming
+# to allow something potentially faulty to exist here, since it would take much longer to find the underlying issue
+# and this is only an experiment.
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 from source.map_generation import map_generation
 
 epochs = 10
-target_dir = "..\\..\\resources\\d_meshes"
-input_dir = "..\\..\\resources\\sketch_meshes"
+target_dir = "..\\..\\resources\\mapgen_dataset\\ABC\\0_499\\n_mapgen"
+input_dir = "..\\..\\resources\\mapgen_dataset\\ABC\\0_499\\sketch_mapgen"
 figure = plt.figure(figsize=(8, 8))
 cols, rows = 1, 2
-dataSet = DataSet.DS(input_dir, target_dir, map_generation.Type.depth)
+dataSet = DataSet.DS(input_dir, target_dir, map_generation.Type.normal)
 dataloader = DataLoader(dataSet, batch_size=1,
                         shuffle=True, num_workers=0)
 for i_batch, sample_batched in enumerate(dataloader):
