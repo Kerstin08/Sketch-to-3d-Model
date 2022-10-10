d04ecfb31939c476ba9ffc74514e4bbbb5682648
Kerstin Hofer <khofer.mmt-m2020@fh-salzburg.ac.at>
2022-10-10 19:00:35 +0200

    Change floodfill to work with 8-connectivity

--- source/topology/floodfill.py
+++ source/topology/floodfill.py
@@ -9,6 +9,9 @@ def startFill(image):
     start_points = find_start_points(image)
     for i in start_points:
         flood_fill_BFS(image, i)
+
+    filled_image = Image.fromarray(image)
+    filled_image.save(r"C:\Users\Kerstin\Documents\MasterThesis\thesis_images\filled_image.png")
     return image
 
 def flood_fill_BFS(image, seed):
@@ -17,6 +20,10 @@ def flood_fill_BFS(image, seed):
 
     while len(stack) > 0:
         x, y = stack.pop()
+        shape_x, shape_y = image.shape
+        if x < 1 or y < 1 or x > shape_x-2 or y > shape_y-2:
+            continue
+
         image[x][y] = fill
         if image[x + 1][y] == background:
             stack.append((x + 1, y))
@@ -29,6 +36,18 @@ def flood_fill_BFS(image, seed):
 
         if image[x][y - 1] == background:
             stack.append((x, y - 1))
+
+        if image[x + 1][y + 1] == background:
+            stack.append((x + 1, y + 1))
+
+        if image[x + 1][y - 1] == background:
+            stack.append((x + 1, y - 1))
+
+        if image[x - 1][y + 1] == background:
+            stack.append((x - 1, y + 1))
+
+        if image[x - 1][y - 1] == background:
+            stack.append((x - 1, y - 1))
 
 def find_start_points(image):
     seeds = deque()
