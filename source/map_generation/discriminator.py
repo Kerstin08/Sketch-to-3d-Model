Fix gradient penalty, and other minor stuff in mapgen.

source/map_generation/discriminator.py
@@ -7,18 +7,15 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.conv1 = nn.Conv2d(2*channel, 64, kernel_size=4, stride=2, padding=1)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
-        self.conv2_in = nn.InstanceNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
-        self.conv3_in = nn.InstanceNorm2d(256)
         self.conv4 = nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1)
-        self.conv4_in = nn.InstanceNorm2d(512)
         self.conv5 = nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1)
         self.lrelu = nn.LeakyReLU(0.2, inplace=True)
 
     def forward(self, x):
         d1 = self.lrelu(self.conv1(x))
-        d2 = self.lrelu(self.conv2_in(self.conv2(d1)))
-        d3 = self.lrelu(self.conv3_in(self.conv3(d2)))
-        d4 = self.lrelu(self.conv4_in(self.conv4(d3)))
+        d2 = self.lrelu(self.conv2(d1))
+        d3 = self.lrelu(self.conv3(d2))
+        d4 = self.lrelu(self.conv4(d3))
         d5 = self.conv5(d4)
         return d5

source/map_generation/main.py
@@ -1,29 +1,30 @@
 import os.path
 
 import map_generation
-import warnings
 import argparse
 from pytorch_lightning.trainer import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 import torch
 from torch.utils.data import DataLoader
-import torch.utils.data as data
-from source.mapgen_dataset import dataset
 from pytorch_lightning.callbacks import ModelCheckpoint
+from source.mapgen_dataset import dataset
+from source.util import data_type
 
 
 def run(train_b, input_dir, output_dir, logs_dir,
         type, epochs, lr, batch_size, n_critic, weight_L1,
+        gradient_penalty_coefficient,
         use_generated_model=False, generated_model_path=""):
     if train_b:
         train(input_dir, output_dir, logs_dir,
         type, epochs, lr, batch_size, n_critic, weight_L1,
-        use_generated_model, generated_model_path)
+        gradient_penalty_coefficient, use_generated_model, generated_model_path)
     else:
         test(input_dir, output_dir, type, batch_size, generated_model_path)
 
 def train(input_dir, output_dir, logs_dir,
         type, epochs, lr, batch_size, n_critic, weight_L1,
+        gradient_penalty_coefficient,
         use_generated_model=False, generated_model_path=""):
 
     sketch_dir = os.path.join(input_dir, "sketch_mapgen")
@@ -42,9 +43,9 @@ def train(input_dir, output_dir, logs_dir,
         os.mkdir(logs_dir)
 
     if type == "depth":
-        given_type = map_generation.Type.depth
+        given_type = data_type.Type.depth
     elif type == "normal":
-        given_type = map_generation.Type.normal
+        given_type = data_type.Type.normal
     else:
         raise Exception("Given type should either be \"normal\" or \"depth\"!")
 
@@ -52,6 +53,7 @@ def train(input_dir, output_dir, logs_dir,
                                   n_critic=n_critic,
                                   batch_size=batch_size,
                                   weight_L1=weight_L1,
+                                  gradient_penalty_coefficient=gradient_penalty_coefficient,
                                   output_dir=output_dir,
                                   lr=lr)
 
@@ -62,6 +64,7 @@ def train(input_dir, output_dir, logs_dir,
                                    n_critic=n_critic,
                                    batch_size=batch_size,
                                    weight_L1=weight_L1,
+                                   gradient_penalty_coefficient=gradient_penalty_coefficient,
                                    output_dir=output_dir,
                                    lr=lr)
 
@@ -124,9 +127,9 @@ def test(input_dir, output_dir,
         os.mkdir(output_dir)
 
     if type == "depth":
-        given_type = map_generation.Type.depth
+        given_type = data_type.Type.depth
     elif type == "normal":
-        given_type = map_generation.Type.normal
+        given_type = data_type.Type.normal
     else:
         raise Exception("Given type should either be \"normal\" or \"depth\"!")
 
@@ -156,6 +159,7 @@ def diff_args(args):
         args.batch_size,
         args.n_critic,
         args.weight_L1,
+        args.gradient_penalty_coefficient,
         args.use_generated_model,
         args.generated_model_path)
 
@@ -174,7 +178,8 @@ def main(args):
     parser.add_argument("--lr", type=float, default=2e-5, help="initial learning rate")
     parser.add_argument("--batch_size", type=int, default=4, help="size of batches")
     parser.add_argument("--n_critic", type=int, default=5, help="# of n_critic")
-    parser.add_argument("--weight_L1", type=int, default=50000, help="L1 weight")
+    parser.add_argument("--weight_L1", type=int, default=500, help="L1 weight")
+    parser.add_argument("--gradient_penalty_coefficient", type=int, default=10, help="gradient penalty coefficient")
     parser.add_argument("--use_generated_model", type=bool, default=False,
                         help="If models are trained from scratch or already trained models are used")
     parser.add_argument("--generated_model_path", type=str, default="..\\..\\output\\test.ckpt",
@@ -189,7 +194,7 @@ if __name__ == '__main__':
         '--type', 'normal',
         '--epochs', '300',
         '--lr', '5e-5',
-        '--output_dir', "checkpoint/176_output",
-        '--generated_model_path', "checkpoint/MapGen-epoch=176-val_loss=0.08904778212308884.ckpt"
+        #'--output_dir', "checkpoint/176_output",
+        #'--generated_model_path', "checkpoint/MapGen-epoch=176-val_loss=0.08904778212308884.ckpt"
     ]
     main(params)

source/map_generation/map_generation.py
@@ -5,18 +5,13 @@ import numpy as np
 from PIL import Image
 import torchvision
 
-from enum import Enum
 from generator import Generator
 from discriminator import Discriminator
 from source.util import OpenEXR_utils
-
-
-class Type(Enum):
-    normal = 1,
-    depth = 2
+from source.util import data_type
 
 class MapGen(pl.LightningModule):
-    def __init__(self, data_type, n_critic, batch_size, weight_L1, output_dir, lr):
+    def __init__(self, data_type, n_critic, batch_size, weight_L1, gradient_penalty_coefficient, output_dir, lr):
         super(MapGen, self).__init__()
         self.save_hyperparameters()
         self.data_type = data_type
@@ -28,10 +23,11 @@ class MapGen(pl.LightningModule):
         self.output_dir = output_dir
         self.lr = lr
         self.L1 = torch.nn.L1Loss()
+        self.gradient_penalty_coefficient = gradient_penalty_coefficient
 
     @property
     def channel(self):
-        if self.data_type == Type.depth:
+        if self.data_type == data_type.Type.depth:
             return 1
         else:
             return 3
@@ -59,6 +55,39 @@ class MapGen(pl.LightningModule):
         self.log("g_Loss", float(g_loss.item()), on_epoch=True, prog_bar=True, logger=True, batch_size=self.batch_size)
         return g_loss
 
+    def gradient_penalty(self, real_images, fake_images):
+        alpha = torch.rand((real_images.size(0), 1, 1, 1)).to(real_images.device)
+        alpha = alpha.expand_as(real_images)
+        interpolation = alpha * real_images + ((1 - alpha) * fake_images).requires_grad_(True)
+        d_interpolated = self.D(interpolation)
+        gradients = torch.autograd.grad(
+            outputs=d_interpolated,
+            inputs=interpolation,
+            grad_outputs=torch.ones_like(d_interpolated),
+            create_graph=True,
+            retain_graph=True,
+        )[0]
+        gradients = gradients.view(real_images.size(0), -1)
+        grad_norm = gradients.norm(2, 1)
+        return torch.mean((grad_norm - 1) ** 2)
+    def compute_gradient_penalty(self, real_samples, fake_samples):
+        alpha = torch.Tensor(np.random.random((real_samples.size(0), 1, 1, 1))).to(self.device)
+        interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
+        interpolates = interpolates.to(self.device)
+        d_interpolates = self.D(interpolates)
+        fake = torch.Tensor(real_samples.shape[0], 1).fill_(1.0).to(self.device)
+        gradients = torch.autograd.grad(
+            outputs=d_interpolates,
+            inputs=interpolates,
+            grad_outputs=fake,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+        gradients = gradients.view(gradients.size(0), -1).to(self.device)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        return gradient_penalty
+
     def discriminator_step(self, sample_batched, fake_images):
         print("Discriminator")
         input_predicted = torch.cat((sample_batched['input'], fake_images), 1)
@@ -68,9 +97,10 @@ class MapGen(pl.LightningModule):
         input_target = torch.cat((sample_batched['input'], sample_batched['target']), 1)
         pred_true = self.D(input_target)
         d_loss_real = torch.mean(pred_true)
+        gradient_penalty = self.gradient_penalty(input_target, input_predicted)
 
         # loss as defined by Wasserstein paper
-        d_loss = -d_loss_real + d_loss_fake
+        d_loss = -d_loss_real + d_loss_fake + self.gradient_penalty_coefficient * gradient_penalty
         self.log("d_loss", float(d_loss.item()), on_epoch=True, prog_bar=True, logger=True, batch_size=self.batch_size)
         self.log("d_loss_real", float(d_loss_real.item()), on_epoch=True, prog_bar=True, logger=True, batch_size=self.batch_size)
         self.log("d_loss_fake", float(d_loss_fake.item()), on_epoch=True, prog_bar=True, logger=True, batch_size=self.batch_size)

source/mapgen_dataset/dataset.py
@@ -4,7 +4,7 @@ from source.util import OpenEXR_utils
 from torchvision import transforms
 from torch.utils.data import Dataset
 from PIL import Image
-from source.map_generation import map_generation
+from source.util import data_type
 
 
 class DS(Dataset):
@@ -32,7 +32,7 @@ class DS(Dataset):
     def __getitem__(self, index):
         # input is sketch, therefore png file
         input_path = self.image_paths_input[index]
-        if self.data_type.value == map_generation.Type.normal.value:
+        if self.data_type == data_type.Type.normal:
             input_image = Image.open(input_path).convert("RGB")
         else:
             input_image = Image.open(input_path).convert("L")
@@ -43,8 +43,8 @@ class DS(Dataset):
         target_path = self.image_paths_target[index]
         target_image = OpenEXR_utils.getRGBimageEXR(target_path, self.data_type, 0)
         target_image_tensor = torch.from_numpy(target_image)
-        if self.data_type.value == map_generation.Type.depth.value:
-            target_image_tensor *= 2 + 1
+        if self.data_type.value == data_type.Type.depth.value:
+            target_image_tensor *= 2 - 1
         return {'input': input_image_tensor,
                 'target': target_image_tensor,
                 'input_path': input_path,